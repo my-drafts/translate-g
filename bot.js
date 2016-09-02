44a336f93577eec868d17b4744b123f334fb08a6
Sergej Kukharev <sergej.kucharev@gmail.com>
2016-09-02 03:48:07 +0300

    1.0.25

--- bot.js
+++ bot.js
@@ -1,4 +1,15 @@
 'use static';
 
+
 var tic = require('./lib/tic');
 
+
+var tics = function(){
+	const time = 1000*120;
+	let sto = function(){
+		setTimeout(tics, time);
+	};
+	tic().then(sto, sto);
+};
+
+tics();
