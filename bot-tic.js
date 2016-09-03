649fc94bac78f0d53a5e7c38b450e3b6364756f3
Sergej Kukharev <sergej.kucharev@gmail.com>
2016-09-04 01:07:28 +0300

    1.0.30a

--- bot-tic.js
+++ bot-tic.js
@@ -4,10 +4,16 @@
 var tic = require('./lib/tic');
 
 
-tic()
-	.then(function(result){
-		console.log('result: ', result);
-	})
-	.catch(function(error){
-		console.log('error: ', error);
-	});
+var run = function(){
+	tic()
+		.then(function(result){
+			console.log('result: ', result);
+		})
+		.catch(function(error){
+			console.log('error: ', error);
+		});
+}
+
+
+setTimeout(run, 1000);
+
